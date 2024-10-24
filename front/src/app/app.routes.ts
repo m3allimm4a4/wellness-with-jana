4d0dd39d96096edd240e0e134a558720aaa10fec
Bechara <b.elrammouz@equadis.com>
2024-10-24 22:41:14 +0300

    fix bugs

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'Home',
+    title: 'Wellness With Jana',
     pathMatch: 'full',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
   },
