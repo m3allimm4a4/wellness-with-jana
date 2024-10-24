7fa358f6d63643a4f68682b6161862a3bb42dd3d
Bechara <b.elrammouz@equadis.com>
2024-10-24 21:20:58 +0300

    fix header

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [MenubarModule, Button],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
