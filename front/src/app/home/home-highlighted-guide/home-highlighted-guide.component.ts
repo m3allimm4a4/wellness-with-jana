ee11c28c2900e9792584ede6ac0710d89f412fa1
Bechara <b.elrammouz@equadis.com>
2024-10-26 19:05:43 +0300

    add home highlited guide page

--- front/src/app/home/home-highlighted-guide/home-highlighted-guide.component.ts
+++ front/src/app/home/home-highlighted-guide/home-highlighted-guide.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { LazyAnimateDirective } from '../../shared/directives/lazy-animate.directive';
 
 @Component({
   selector: 'app-home-highlighted-guide',
   standalone: true,
-  imports: [],
+  imports: [Button, LazyAnimateDirective],
   templateUrl: './home-highlighted-guide.component.html',
   styleUrl: './home-highlighted-guide.component.scss',
 })
