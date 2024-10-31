c80a2c6a2c9492554202e63729e781c6f0f782d1
Bechara <b.elrammouz@equadis.com>
2024-10-31 21:17:28 +0200

    add .env

--- back/index.ts
+++ back/index.ts
@@ -1,11 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import { NotFoundError } from './errors/not-found.error';
 import { errorHandler } from './middlewares/error.handler';
+import mongoose from 'mongoose';
 
 const run = async () => {
+  const env = process.env.NODE_ENV || 'dev';
+  dotenv.config({ path: `./environment/.${env}.env` });
+
   const server = express();
+
+  await mongoose.connect(process.env.DATABASE_URL || '');
 
   server.use(cors());
 
@@ -19,15 +26,20 @@ const run = async () => {
     }),
   );
 
+  server.get('/api/test', (_req, res) => {
+    res.status(200).send('API is running');
+  });
+
   server.get('/api/**', (_req, _res) => {
     throw new NotFoundError();
   });
 
   server.use(errorHandler);
 
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT) || 3000;
   server.listen(port, () => {
-    if (!process.env.PROD) {
+    const prod = process.env.PROD === 'true';
+    if (prod) {
       console.log(`Node Express server listening on http://localhost:${port}/api`);
     }
   });
