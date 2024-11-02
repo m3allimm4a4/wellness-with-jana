b8ac2f70c7f26d74b2d114c63dffa4971376e7ff
Bechara <b.elrammouz@equadis.com>
2024-11-02 17:35:06 +0200

    add admin services list page

--- front/src/app/admin/admin-home/admin-home-services-list/admin-home-services-list.component.ts
+++ front/src/app/admin/admin-home/admin-home-services-list/admin-home-services-list.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { Button } from 'primeng/button';
+import { EditorModule } from 'primeng/editor';
+import { InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { LabelsService } from '../../services/labels.service';
 
 @Component({
   selector: 'app-admin-home-services-list',
   standalone: true,
-  imports: [],
+  imports: [CardModule, Button, EditorModule, InputTextModule, PaginatorModule, ReactiveFormsModule],
   templateUrl: './admin-home-services-list.component.html',
-  styleUrl: './admin-home-services-list.component.scss'
+  styleUrl: './admin-home-services-list.component.scss',
 })
-export class AdminHomeServicesListComponent {
+export class AdminHomeServicesListComponent implements OnInit, OnDestroy {
+  servicesListForm = new FormGroup({
+    title: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    description: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+  });
 
+  private subscription = new Subscription();
+
+  constructor(private labelsService: LabelsService) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this.labelsService.getLabels().subscribe(labels => {
+        this.servicesListForm.patchValue({
+          title: labels.get('home-services-list-title'),
+          description: labels.get('home-services-list-description'),
+        });
+      }),
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  saveServicesList() {
+    this.subscription.add(
+      this.labelsService
+        .createOrUpdateLabels([
+          { name: 'home-services-list-title', en: this.servicesListForm.controls.title.value },
+          { name: 'home-services-list-description', en: this.servicesListForm.controls.description.value },
+        ])
+        .subscribe(),
+    );
+  }
 }
--- front/src/app/home/home-services-list/home-services-list.component.ts
+++ front/src/app/home/home-services-list/home-services-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeServicesListItemComponent } from './home-services-list-item/home-services-list-item.component';
 import { LazyAnimateDirective } from '../../shared/directives/lazy-animate.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-services-list',
   standalone: true,
-  imports: [HomeServicesListItemComponent, LazyAnimateDirective],
+  imports: [HomeServicesListItemComponent, LazyAnimateDirective, TranslateModule],
   templateUrl: './home-services-list.component.html',
   styleUrl: './home-services-list.component.scss',
 })
