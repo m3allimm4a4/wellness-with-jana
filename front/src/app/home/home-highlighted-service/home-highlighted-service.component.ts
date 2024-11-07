db83f640bd539df5f863a250786408244e20cf80
Bechara <b.elrammouz@equadis.com>
2024-11-07 19:02:41 +0200

    refactor highlighted service

--- front/src/app/home/home-highlighted-service/home-highlighted-service.component.ts
+++ front/src/app/home/home-highlighted-service/home-highlighted-service.component.ts
@@ -1,24 +1,14 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Button } from 'primeng/button';
 import { LazyAnimateDirective } from '../../shared/directives/lazy-animate.directive';
 import { TranslateModule } from '@ngx-translate/core';
-import { Asset, AssetType } from '../../shared/interfaces/asset.interface';
-import { AssetsService } from '../../shared/services/assets.service';
+import { AssetComponent } from '../../shared/components/asset/asset.component';
 
 @Component({
   selector: 'app-home-highlighted-service',
   standalone: true,
-  imports: [Button, LazyAnimateDirective, TranslateModule],
+  imports: [Button, LazyAnimateDirective, TranslateModule, AssetComponent],
   templateUrl: './home-highlighted-service.component.html',
   styleUrl: './home-highlighted-service.component.scss',
 })
-export class HomeHighlightedServiceComponent implements OnInit {
-  protected readonly AssetType = AssetType;
-  protected asset = signal<Asset | undefined>(undefined);
-
-  constructor(private assetsService: AssetsService) {}
-
-  ngOnInit() {
-    this.assetsService.getAssetById('home-highlighted-service').subscribe(asset => this.asset.set(asset));
-  }
-}
+export class HomeHighlightedServiceComponent {}
--- front/src/app/shared/components/asset/asset.component.ts
+++ front/src/app/shared/components/asset/asset.component.ts
@@ -1,18 +1,19 @@
 import { Component, effect, input, signal } from '@angular/core';
 import { Asset, AssetType } from '../../interfaces/asset.interface';
 import { AssetsService } from '../../services/assets.service';
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-asset',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './asset.component.html',
   styleUrl: './asset.component.scss',
 })
 export class AssetComponent {
   assetId = input.required<string>();
   style = input<Record<string, string>>();
+  styleClasses = input<Record<string, any> | string | string[]>();
 
   protected readonly AssetType = AssetType;
   protected asset = signal<Asset | undefined>(undefined);
