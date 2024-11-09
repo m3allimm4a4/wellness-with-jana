ef9d556dcfeb5bea8d9b871a0127faa345130a28
Bechara <b.elrammouz@equadis.com>
2024-11-09 16:32:39 +0200

    add some sections to contact page

--- front/src/app/contact/contact.component.ts
+++ front/src/app/contact/contact.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ContactBannerComponent } from './contact-banner/contact-banner.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HomeTrialComponent } from '../home/home-trial/home-trial.component';
+import { HomeSocialShowcaseComponent } from '../home/home-social-showcase/home-social-showcase.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ContactBannerComponent, ContactFormComponent],
+  imports: [ContactBannerComponent, ContactFormComponent, HomeTrialComponent, HomeSocialShowcaseComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
