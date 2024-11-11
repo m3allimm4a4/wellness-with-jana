54837e7b9d06eaf183133f1349f26e4ab2148abd
Bechara <b.elrammouz@equadis.com>
2024-11-11 20:38:10 +0200

    add about description

--- front/src/app/about/about-description/about-description.component.ts
+++ front/src/app/about/about-description/about-description.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { LazyAnimateDirective } from '../../shared/directives/lazy-animate.directive';
 
 @Component({
   selector: 'app-about-description',
   standalone: true,
-  imports: [],
+  imports: [Button, LazyAnimateDirective],
   templateUrl: './about-description.component.html',
-  styleUrl: './about-description.component.scss'
+  styleUrl: './about-description.component.scss',
 })
-export class AboutDescriptionComponent {
-
-}
+export class AboutDescriptionComponent {}
