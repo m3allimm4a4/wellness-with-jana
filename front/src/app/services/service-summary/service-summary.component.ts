fe30f4d90fac5286b20e54171bdcdcc9d3aca39b
Bechara <b.elrammouz@equadis.com>
2024-11-22 13:48:38 +0200

    add services summary

--- front/src/app/services/service-summary/service-summary.component.ts
+++ front/src/app/services/service-summary/service-summary.component.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Service } from '../../shared/interfaces/service.interface';
 import { RouterLink } from '@angular/router';
+import { LazyAnimateDirective } from '../../shared/directives/lazy-animate.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-service-summary',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LazyAnimateDirective, TranslateModule],
   templateUrl: './service-summary.component.html',
   styleUrl: './service-summary.component.scss',
 })
