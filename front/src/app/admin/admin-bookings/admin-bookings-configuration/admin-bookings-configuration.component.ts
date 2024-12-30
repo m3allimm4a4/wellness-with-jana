96c14b6a4eb53460c37644cf172c20a55ececb4f
Bechara <b.elrammouz@equadis.com>
2024-12-30 16:17:24 +0200

    save booking dynamic config

--- front/src/app/admin/admin-bookings/admin-bookings-configuration/admin-bookings-configuration.component.ts
+++ front/src/app/admin/admin-bookings/admin-bookings-configuration/admin-bookings-configuration.component.ts
@@ -3,11 +3,12 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DynamicConfigService } from '../../services/dynamic-config.service';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-admin-bookings-configuration',
   standalone: true,
-  imports: [CardModule, InputTextModule, ReactiveFormsModule],
+  imports: [CardModule, InputTextModule, ReactiveFormsModule, Button],
   templateUrl: './admin-bookings-configuration.component.html',
   styleUrl: './admin-bookings-configuration.component.scss',
 })
@@ -22,8 +23,33 @@ export class AdminBookingsConfigurationComponent implements OnInit {
   constructor(private dynamicConfigService: DynamicConfigService) {}
 
   ngOnInit() {
+    this.bookingConfigForm.controls.start.valueChanges.subscribe(start => {
+      const validators = [Validators.required];
+      if (start) {
+        validators.push(control => (control.value < start ? { error: 'invalid' } : null));
+      }
+      this.bookingConfigForm.controls.end.setValidators(validators);
+    });
+    this.bookingConfigForm.controls.end.valueChanges.subscribe(end => {
+      const validators = [Validators.required];
+      if (end) {
+        validators.push(control => (control.value > end ? { error: 'invalid' } : null));
+      }
+      this.bookingConfigForm.controls.start.setValidators(validators);
+    });
     this.dynamicConfigService.getConfigByName('APPOINTMENT').subscribe(config => {
       this.bookingConfigForm.patchValue(config);
     });
+  }
+
+  onBookingConfigSave() {
+    this.dynamicConfigService
+      .saveConfig('APPOINTMENT', {
+        start: this.bookingConfigForm.value.start,
+        end: this.bookingConfigForm.value.end,
+        duration: this.bookingConfigForm.value.duration,
+        spacing: this.bookingConfigForm.value.spacing,
+      })
+      .subscribe();
   }
 }
--- front/src/app/admin/services/dynamic-config.service.ts
+++ front/src/app/admin/services/dynamic-config.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { MessageService } from 'primeng/api';
+import { tap } from 'rxjs';
 
 @Injectable()
 export class DynamicConfigService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) {}
 
   getConfigByName(name: string) {
     return this.http.get<Record<string, any>>(`${environment.apiUrl}/dynamic-config/${name}`);
+  }
+
+  saveConfig(name: string, config: Record<string, any>) {
+    return this.http
+      .post(`${environment.apiUrl}/dynamic-config/${name}`, config)
+      .pipe(tap(() => this.createSuccessToast('Settings updated successfully')));
+  }
+
+  private createSuccessToast(message: string): void {
+    this.messageService.add({
+      key: 'toast',
+      severity: 'success',
+      summary: message,
+    });
   }
 }
