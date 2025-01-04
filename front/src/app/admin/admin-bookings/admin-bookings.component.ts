e907d983b478825c1713b4db8e073a113b054dde
Bechara <b.elrammouz@equadis.com>
2025-01-04 11:48:24 +0200

    add bookings list

--- front/src/app/admin/admin-bookings/admin-bookings.component.ts
+++ front/src/app/admin/admin-bookings/admin-bookings.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {
   AdminBookingsConfigurationComponent
 } from './admin-bookings-configuration/admin-bookings-configuration.component';
+import { AdminBookingsListComponent } from './admin-bookings-list/admin-bookings-list.component';
 
 @Component({
   selector: 'app-admin-bookings',
   standalone: true,
-  imports: [AdminBookingsConfigurationComponent],
+  imports: [AdminBookingsConfigurationComponent, AdminBookingsListComponent],
   templateUrl: './admin-bookings.component.html',
   styleUrl: './admin-bookings.component.scss',
 })
